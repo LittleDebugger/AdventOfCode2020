1febb290e2319704e7e086c5d14b5e1c7b7c2762
littledebugger <hedley.gisbourne@googlemail.com>
2020-12-12 09:52:42 +0100

    Day1: Part 1 & 2 - Improvements

--- Day1/Part1.py
+++ Day1/Part1.py
@@ -1,11 +1,2 @@
-file = [int(l.strip()) for l in open("c:\\aoc\\2020\\Day1.txt")]
-
-for a in range(0, len(file)):
-    if file[a] > 2020:
-        continue
-    for b in range(a + 1, len(file)):
-        if file[a] == file[b]:
-            continue
-        if file[a] + file[b] == 2020:
-            print(file[a], file[b], file[a] * file[b])
-            exit()
+from itertools import combinations
+print([i[0] * i[1] for i in combinations([int(l.strip()) for l in open("c:\\aoc\\2020\\Day1.txt")], 3) if i[0] + i[1] == 2020][0])
