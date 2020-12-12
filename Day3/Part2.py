e92501d93e457335a3083019ed4db2af09f2ad46
littledebugger <hedley.gisbourne@googlemail.com>
2020-12-12 10:27:50 +0100

    Day3: Part 2 - Improvelement

--- Day3/Part2.py
+++ Day3/Part2.py
@@ -1,8 +1,3 @@
-#from collections import Counter
-#from collections import namedtuple as nt
-#from collections import defaultdict as dd
-import re # regex
-
 tt = 1
 
 for xx, yy in [[1,1], [3,1], [5,1], [7, 1], [1, 2]]:
@@ -12,17 +7,15 @@ for xx, yy in [[1,1], [3,1], [5,1], [7, 1], [1, 2]]:
     t = 0
 
     while y < len(file):
+        if file[y][x % len(file[y])] == '#':
+            t += 1
         x += xx
         y += yy
-        try:
-            if file[y][x % len(file[y])] == '#':
-                t += 1
-        except:
-            pass
-    print(t)
+
     tt = tt * t
 
 print (tt)
+
 
 
 
