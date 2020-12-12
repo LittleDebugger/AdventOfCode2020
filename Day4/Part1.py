d343fc94cde3cf9ada850ba4bef1b7570b193e40
littledebugger <hedley.gisbourne@googlemail.com>
2020-12-12 10:38:53 +0100

    Day4: Part 1 - Clean-up

--- Day4/Part1.py
+++ Day4/Part1.py
@@ -1,16 +1,12 @@
-from collections import Counter
-#from collections import namedtuple as nt
-#from collections import defaultdict as dd
-
 file = [l.strip() for l in open("c:\\aoc\\2020\\Day4.txt")]
 
 required = ['byr',
-'iyr',
-'eyr',
-'hgt',
-'hcl',
-'ecl',
-'pid' ]
+            'iyr',
+            'eyr',
+            'hgt',
+            'hcl',
+            'ecl',
+            'pid']
 
 t = 0
 person = []
@@ -19,9 +15,7 @@ for l in file:
         person = []
         continue
     for ll in l.split(' '):
-        #print(ll)
         word = ll[:3]
-        print(word)
         if word in required and word not in person:
             person.append(word)
     if len(person) >= len(required):
