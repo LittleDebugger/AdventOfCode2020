37643e1e8d8e77ad29b18dd706e7e346910fb5cd
littledebugger <hedley.gisbourne@googlemail.com>
2020-12-12 10:23:08 +0100

    Day3: Part 1 - Clean-up

--- Day3/Part1.py
+++ Day3/Part1.py
@@ -1,10 +1,4 @@
-#from collections import Counter
-#from collections import namedtuple as nt
-#from collections import defaultdict as dd
-import re # regex
-
 file = [l.strip() for l in open("c:\\aoc\\2020\\Day3.txt")]
-#print(file[0])
 
 y = 0
 x = 0
---
+++ Day3/Part2.py
@@ -0,0 +1,29 @@
+#from collections import Counter
+#from collections import namedtuple as nt
+#from collections import defaultdict as dd
+import re # regex
+
+
+tt = 1
+
+for xx, yy in [[1,1], [3,1], [5,1], [7, 1], [1, 2]]:
+    file = [l.strip() for l in open("c:\\aoc\\2020\\Day3.txt")]
+    y = 0
+    x = 0
+    t = 0
+
+    while y < len(file):
+        x += xx
+        y += yy
+        try:
+            if file[y][x % len(file[y])] == '#':
+                t += 1
+        except:
+            pass
+    print(t)
+    tt = tt * t
+
+print (tt)
+
+
+
