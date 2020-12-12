048061f5c15b669e85c7f5f8791ce4453d7b5608
littledebugger <hedley.gisbourne@googlemail.com>
2020-12-12 10:16:10 +0100

    Day2: Part 2 - Clean-up

--- Day2/Part2.py
+++ Day2/Part2.py
@@ -1,10 +1,4 @@
-#from collections import Counter
-#from collections import namedtuple as nt
-#from collections import defaultdict as dd
-
-
 file = [l.strip() for l in open("c:\\aoc\\2020\\Day2.txt")]
-print(file[0])
 
 t = 0
 for l in file:
@@ -14,7 +8,7 @@ for l in file:
     word = b[2]
     count = word.count(letter)
     try:
-        if (word[int(m[0]) - 1] == letter and word[int(m[1]) - 1] != letter) or (word[int(m[0]) - 1] != letter and word[int(m[1]) - 1]  == letter):
+        if (word[int(m[0]) - 1] == letter and word[int(m[1]) - 1] != letter) or (word[int(m[0]) - 1] != letter and word[int(m[1]) - 1] == letter):
             t += 1
     except:
         pass
