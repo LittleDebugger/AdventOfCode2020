e276970513e2dc877b873e95d2facdd033ce28db
littledebugger <hedley.gisbourne@googlemail.com>
2020-12-12 10:11:51 +0100

    Day2: Part 1 - Clean-up

--- Day2/Part1.py
+++ Day2/Part1.py
@@ -1,9 +1,4 @@
-#from collections import Counter
-#from collections import namedtuple as nt
-#from collections import defaultdict as dd
-
-file = [l.strip() for l in open("c:\\aoc\\2020\\Day2.txt")]
-print(file[0])
+file = [line.strip() for line in open("c:\\aoc\\2020\\Day2.txt")]
 
 t = 0
 for l in file:
@@ -12,6 +7,6 @@ for l in file:
     letter = b[1][0]
     word = b[2]
     count = word.count(letter)
-    if count >= int(m[0]) and count <= int(m[1]):
+    if int(m[0]) <= count <= int(m[1]):
         t += 1
 print(t)
